6b41d7d67615895fc6bafe7ef4252df2f783f34b
ayeooong <ayeong0115@naver.com>
2025-03-05 09:39:07 +0900

    design: 냉장고 식재료 직접 입력하기 페이지 UI 설계 및 디자인

---
+++ src/pages/refrigerator/IngredientAddForm.js
@@ -0,0 +1,129 @@
+import "./IngredientAddForm.css";
+import Header from "../../components/common/header/Header";
+import backArrow from "../../assets/backArrow.svg";
+import close from "../../assets/close.svg";
+import Input from "../../components/common/input/Input";
+import Select from "../../components/common/select/Select";
+import ButtonL from "../../components/common/button/ButtonL";
+import Menu from "../../components/common/menu/Menu";
+import imagePlus from "../../assets/imagePlus.svg";
+import { useRef, useState } from "react";
+
+const IngredientAddForm = () => {
+  // 이미지 미리보기 상태 관리
+  const [imagePreview, setImagePreview] = useState(null);
+  // 파일 입력 요소 접근
+  const fileInputRef = useRef(null);
+
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const mainOptions = [
+    { value: "", label: "대분류" },
+    { value: "option1", label: "옵션 1" },
+    { value: "option2", label: "옵션 2" },
+    { value: "option3", label: "옵션 3" },
+  ];
+  const subOptions = [
+    { value: "", label: "중분류" },
+    { value: "option1", label: "옵션 1" },
+    { value: "option2", label: "옵션 2" },
+    { value: "option3", label: "옵션 3" },
+  ];
+  const detailOptions = [
+    { value: "", label: "소분류" },
+    { value: "option1", label: "옵션 1" },
+    { value: "option2", label: "옵션 2" },
+    { value: "option3", label: "옵션 3" },
+  ];
+
+  return (
+    <>
+      <Header
+        leftIcon={backArrow}
+        title="직접 입력하기"
+        rightIcon={close}
+        onLeftClick={() => window.history.back()}
+        onRightClick={() => window.history.back()}
+      />
+
+      <div className="input-wrapper">
+        <div className="input-container">
+          <h3 className="input-label bold">식재료명</h3>
+          <Input className="input" placeholder="식재료명을 입력해 주세요." />
+        </div>
+
+        <div className="input-container">
+          <h3 className="input-label bold">사진 첨부</h3>
+          <div
+            className="image-upload-container"
+            onClick={handleImageClick}
+            style={{
+              backgroundImage: imagePreview ? `url(${imagePreview})` : "none",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            {!imagePreview && (
+              <img
+                src={imagePlus}
+                alt="이미지 추가"
+                className="image-plus-icon"
+              />
+            )}
+            {/* 파일 입력 요소 */}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              ref={fileInputRef}
+              style={{ display: "none" }}
+            />
+          </div>
+        </div>
+
+        <div className="input-container">
+          <h3 className="input-label bold">유통기한</h3>
+          <Input className="input" type="date" />
+        </div>
+
+        <div className="input-container">
+          <h3 className="input-label bold">분류기준</h3>
+          <div className="select-container">
+            <Select options={mainOptions} className="select-item" />
+            <Select options={subOptions} className="select-item" />
+            <Select options={detailOptions} className="select-item" />
+          </div>
+        </div>
+
+        <div className="input-container">
+          <h3 className="input-label bold">중량</h3>
+          <Input className="input" placeholder="중량을 입력해 주세요." />
+        </div>
+      </div>
+
+      <div className="button-container">
+        <ButtonL text="취소" variant="outlined" />
+        <ButtonL text="등록하기" />
+      </div>
+
+      <div style={{ height: "100px" }}></div>
+
+      <Menu />
+    </>
+  );
+};
+
+export default IngredientAddForm;
