4c25eb3c86ded80ed2d2d7f3786126aa550cc817
ayeooong <ayeong0115@naver.com>
2025-03-06 11:31:49 +0900

    design: 메인 페이지에 로그인 유도 배너 추가

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Noti from "./pages/noti/Noti";
 import IngredientAddForm from "./pages/refrigerator/IngredientAddForm";
 import IngredientDetailList from "./pages/refrigerator/IngredientDetailList";
 import CardIssuePage from "./pages/card/CardIssuePage"; // 카드 발급 안내 페이지
-import CardApplyPage from "./pages/card/CardApplyPage"; // 카드 신청 페이지
-import TemplatePage from "./pages/template/TemplatePage"; // 템플릿 페이지
+// import CardApplyPage from "./pages/card/CardApplyPage"; // 카드 신청 페이지
+// import TemplatePage from "./pages/template/TemplatePage"; // 템플릿 페이지
+import RefrigeratorMain from "./pages/refrigerator/RefrigeratorMain";
 
 function App() {
   return (
@@ -22,8 +23,10 @@ function App() {
           path="/ingredientdetaillist"
           element={<IngredientDetailList />}
         />
+        <Route path="/refrigeratormain" element={<RefrigeratorMain />} />
+
         {/* 카드 신청 관련 라우트 */}
-        <Route path="/template" element={<TemplatePage />} />
+        {/* <Route path="/template" element={<TemplatePage />} /> */}
         <Route path="/card" element={<CardIssuePage />} />
         {/* <Route path="/card/apply" element={<CardApplyPage />} /> */}
       </Route>
