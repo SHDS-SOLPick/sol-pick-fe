27cd785e83a2941edd2ab3a3a72c751e827fb604
ayeooong <ayeong0115@naver.com>
2025-03-06 17:16:45 +0900

    design: 냉장고 페이지 및 식재료 등록 팝업 UI 설계 및 디자인

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Main from "./pages/main/Main";
 import Noti from "./pages/noti/Noti";
 import IngredientAddForm from "./pages/refrigerator/IngredientAddForm";
 import IngredientDetailList from "./pages/refrigerator/IngredientDetailList";
-import CardIssuePage from "./pages/card/CardIssuePage"; // 카드 발급 안내 페이지
+import RefrigeratorMain from "./pages/refrigerator/RefrigeratorMain";
+// import CardIssuePage from "./pages/card/CardIssuePage"; // 카드 발급 안내 페이지
 // import CardApplyPage from "./pages/card/CardApplyPage"; // 카드 신청 페이지
 // import TemplatePage from "./pages/template/TemplatePage"; // 템플릿 페이지
-import RefrigeratorMain from "./pages/refrigerator/RefrigeratorMain";
 
 function App() {
   return (
@@ -27,7 +27,7 @@ function App() {
 
         {/* 카드 신청 관련 라우트 */}
         {/* <Route path="/template" element={<TemplatePage />} /> */}
-        <Route path="/card" element={<CardIssuePage />} />
+        {/* <Route path="/card" element={<CardIssuePage />} /> */}
         {/* <Route path="/card/apply" element={<CardApplyPage />} /> */}
       </Route>
     </Routes>
--- src/components/refrigerator/IngredientItem.js
+++ src/components/refrigerator/IngredientItem.js
@@ -1,18 +1,32 @@
+import React from "react";
 import "./IngredientItem.css";
 
-const IngredientItem = ({ image, category, name, addDate, onClick }) => {
+const IngredientItem = ({ ingredient, onClick }) => {
   return (
-    <>
-      <div className="ingredient-item-container" onClick={onClick}>
-        <img src={image} alt={name} className="ingredient-item-image" />
-
-        <div className="ingredient-item-detail">
-          <p className="ingredient-category">{category}</p>
-          <h3 className="ingredient-name bold">{name}</h3>
-          <p className="ingredient-add-date">{addDate}</p>
-        </div>
-      </div>
-    </>
+    <div
+      key={ingredient.id}
+      className="ingredient-item"
+      onClick={() => onClick(ingredient)}
+      style={{
+        position: "absolute",
+        left: `${ingredient.x * (320 / 343)}px`, // 냉장고 비율에 맞게 조정
+        width: `${ingredient.size}px`,
+        height: `${ingredient.size}px`,
+        cursor: "pointer",
+      }}
+    >
+      <img
+        src={ingredient.image}
+        alt={ingredient.name}
+        className="ingredient-image"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+        }}
+      />
+    </div>
   );
 };
+
 export default IngredientItem;
