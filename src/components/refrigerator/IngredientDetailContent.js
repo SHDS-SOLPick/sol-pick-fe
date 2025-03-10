57a8f8d016563d6898127d2d8e48ae19e4fb4c09
ayeooong <ayeong0115@naver.com>
2025-03-10 23:11:56 +0900

    refactor: 전달된 데이터 없을 때 기본값 설정

--- src/components/refrigerator/IngredientDetailContent.js
+++ src/components/refrigerator/IngredientDetailContent.js
@@ -2,49 +2,58 @@ import React from "react";
 import detailArrow from "../../assets/detailArrow.svg";
 import "./IngredientDetailContent.css";
 
-const IngredientDetailContent = ({ ingredient }) => {
-  if (!ingredient) return null;
-
+const IngredientDetailContent = ({
+  ingredient = {
+    name: "식재료명 정보 없음",
+    attachedImage: "/path/to/default-image.png",
+    expiryDate: "유통기한 정보 없음",
+    weight: "보유량 정보 없음",
+    addDate: "등록일 정보 없음",
+  },
+}) => {
   return (
     <div className="ingpopup-description-container">
       <div className="ingpopup-description-category">
         <p className="ingmain-category">대분류</p>
         <img
           src={detailArrow}
-          alt="상세화살표"
+          alt="detailArrow"
           className="ingdetailArrow-icon"
         />
         <p className="ingsub-category">중분류</p>
         <img
           src={detailArrow}
-          alt="상세화살표"
+          alt="detailArrow"
           className="ingdetailArrow-icon"
         />
         <p className="ingdetail-category">소분류</p>
       </div>
 
-      {/* 첨부된 이미지 표시 (없으면 기본 이미지나 빈 공간) */}
       <img
         src={ingredient.attachedImage || "/path/to/default-image.png"}
-        alt={ingredient.name}
+        alt={ingredient.name || "식재료명 정보 없음"}
         className="ingpopup-description-image"
       />
 
       <div className="ingpopup-description">
         <p className="ingpopup-description-label">유통기한</p>
         <p className="ingpopup-description-expiration">
-          {ingredient.expiryDate}
+          {ingredient.expiryDate || "유통기한 정보 없음"}
         </p>
       </div>
 
       <div className="ingpopup-description">
         <p className="ingpopup-description-label">보유량</p>
-        <p className="ingpopup-description-gram">{ingredient.weight}</p>
+        <p className="ingpopup-description-gram">
+          {ingredient.weight || "보유량 정보 없음"}
+        </p>
       </div>
 
       <div className="ingpopup-description">
         <p className="ingpopup-description-label">등록일</p>
-        <p className="ingpopup-description-adddate">{ingredient.addDate}</p>
+        <p className="ingpopup-description-adddate">
+          {ingredient.addDate || "등록일 정보 없음"}
+        </p>
       </div>
     </div>
   );
