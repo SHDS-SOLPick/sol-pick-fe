8f29b1c4d941cac708525e5e2dff3b019419bb0d
jrkim-kr <jeongrankim99@gmail.com>
2025-03-17 03:13:11 +0900

    style(card): 커스텀 카드 배경 디자인 수정 #32

--- src/components/card/CardCustomBackground.js
+++ src/components/card/CardCustomBackground.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import "./CardCustomBackground.css";
+import CardBackground1 from "../../assets/card/cardBackground1.svg";
+import CardBackground2 from "../../assets/card/cardBackground2.svg";
+import CardBackground3 from "../../assets/card/cardBackground3.svg";
+import CardBackground4 from "../../assets/card/cardBackground4.svg";
 
 const CardCustomBackground = ({ onNext }) => {
-  // 카드 디자인 옵션 데이터
+  // 카드 디자인 옵션 데이터 - SVG 배경으로 변경
   const cardDesigns = [
-    { id: 1, name: "디자인 1", color: "gradient-blue" },
-    { id: 2, name: "디자인 2", color: "gradient-purple" },
-    { id: 3, name: "디자인 3", color: "gradient-pink" },
-    { id: 4, name: "디자인 4", color: "gradient-green" },
+    { id: 1, name: "디자인 1", backgroundImage: CardBackground1 },
+    { id: 2, name: "디자인 2", backgroundImage: CardBackground2 },
+    { id: 3, name: "디자인 3", backgroundImage: CardBackground3 },
+    { id: 4, name: "디자인 4", backgroundImage: CardBackground4 },
   ];
 
   // 선택된 카드 디자인 상태 관리
@@ -31,7 +35,14 @@ const CardCustomBackground = ({ onNext }) => {
                 }`}
                 onClick={() => setSelectedDesign(design.id)}
               >
-                <div className={`bg-card-preview ${design.color}`}></div>
+                <div
+                  className="bg-card-preview"
+                  style={{
+                    backgroundImage: `url(${design.backgroundImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                ></div>
               </div>
             ))}
           </div>
