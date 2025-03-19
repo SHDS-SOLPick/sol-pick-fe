8c7eedd769b945e7756f9ceb9e7d4da051d0efab
eunjiko <e.melies1o14@gmail.com>
2025-03-19 17:51:06 +0900

    refactor: 레시피, 메인, 코드 페이지 리팩토링 #92

--- src/App.js
+++ src/App.js
@@ -19,16 +19,16 @@ import RecipeRecommendation from "./pages/refrigerator/recommendation/RecipeReco
 import RecipeLoading from "./pages/refrigerator/recommendation/RecipeLoading";
 import RecipeDetail from "./pages/refrigerator/recommendation/RecipeDetail";
 
-import CardIssuePage from "./pages/card/CardIssuePage"; // 카드 발급 안내 페이지
-import CardDetailPage from "./pages/card/CardDetailPage"; // 카드 상세 페이지
-import CardDesignSelectionPage from "./pages/card/CardDesignSelectionPage"; // 카드 디자인 선택 페이지
-import CardCustomBackgroundPage from "./pages/card/CardCustomBackgroundPage"; // 카드 배경 선택 페이지
-import CardCustomStickerPage from "./pages/card/CardCustomStickerPage"; // 카드 스티커 선택 페이지
-import CardIdentityVerificationPage from "./pages/card/CardIdentityVerificationPage"; // 본인 인증 페이지
-import CardTermsPage from "./pages/card/CardTermsPage"; // 약관 동의 페이지
-import CardCreditRatingPage from "./pages/card/CardCreditRatingPage"; // 신용 정보 확인 페이지
-import CardApplyInfoPage from "./pages/card/CardApplyInfoPage"; // 카드 신청 정보 입력 페이지
-import CardCompletionPage from "./pages/card/CardCompletionPage"; // 카드 발급 완료 페이지 추가
+// import CardIssuePage from "./pages/card/CardIssuePage"; // 카드 발급 안내 페이지
+// import CardDetailPage from "./pages/card/CardDetailPage"; // 카드 상세 페이지
+// import CardDesignSelectionPage from "./pages/card/CardDesignSelectionPage"; // 카드 디자인 선택 페이지
+// import CardCustomBackgroundPage from "./pages/card/CardCustomBackgroundPage"; // 카드 배경 선택 페이지
+// import CardCustomStickerPage from "./pages/card/CardCustomStickerPage"; // 카드 스티커 선택 페이지
+// import CardIdentityVerificationPage from "./pages/card/CardIdentityVerificationPage"; // 본인 인증 페이지
+// import CardTermsPage from "./pages/card/CardTermsPage"; // 약관 동의 페이지
+// import CardCreditRatingPage from "./pages/card/CardCreditRatingPage"; // 신용 정보 확인 페이지
+// import CardApplyInfoPage from "./pages/card/CardApplyInfoPage"; // 카드 신청 정보 입력 페이지
+// import CardCompletionPage from "./pages/card/CardCompletionPage"; // 카드 발급 완료 페이지 추가
 
 import GameEntryHandler from "./pages/game/GameEntryHandler"; // 게임 진입 처리 컴포넌트
 import GameInitPage from "./pages/game/RecipeSelectionPage"; // 레시피 선택 페이지
@@ -98,38 +98,7 @@ function App() {
           />
           <Route path="/recipe-detail/:id" element={<RecipeDetail />} />
 
-          {/* 카드 신청 관련 라우트 */}
-          <Route path="/card" element={<CardIssuePage />} />
-          <Route path="/card/detail" element={<CardDetailPage />} />
-          <Route
-            path="/card/apply/design"
-            element={<CardDesignSelectionPage />}
-          />
-          <Route
-            path="/card/apply/custom/background"
-            element={<CardCustomBackgroundPage />}
-          />
-          <Route
-            path="/card/apply/custom/sticker"
-            element={<CardCustomStickerPage />}
-          />
-          <Route
-            path="/card/apply/identity-verification"
-            element={<CardIdentityVerificationPage />}
-          />
-          <Route path="/card/apply/terms" element={<CardTermsPage />} />
-          <Route
-            path="/card/apply/credit-rating"
-            element={<CardCreditRatingPage />}
-          />
-          <Route
-            path="/card/apply/apply-info"
-            element={<CardApplyInfoPage />}
-          />
-          <Route
-            path="/card/apply/completion"
-            element={<CardCompletionPage />}
-          />
+
 
           {/* 미니 게임 관련 라우트 */}
           <Route path="/game" element={<GameEntryHandler />} />
